faa147434a2e42065817833a114cf274ea3e2748
tomasyovino <tomasyovino@gmail.com>
2022-09-23 16:13:56 -0300

    Add Cart.js component and Cart Logic

--- src/components/pages/Store/StoreNavbar.js
+++ src/components/pages/Store/StoreNavbar.js
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 import { ShoppingCart } from '@mui/icons-material';
+import { Cart } from "../../exports";
+import { useStateContext } from "../../../context/StateContext";
 
 const StoreNavbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="header-top">
         <Link to="/" className="left">
-            <h1>BOOM</h1>
-            <h4>GangWars</h4>
+          <h1>BOOM</h1>
+          <h4>GangWars</h4>
         </Link>
-        <span className="right"><ShoppingCart /></span>
+        <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+          <ShoppingCart />
+          <span className="cart-item-qty">{totalQuantities}</span>
+        </button>
+
+        { showCart && <Cart /> }
     </div>
   )
 }
--- src/components/pages/Store/sections/Cart.js
+++ src/components/pages/Store/sections/Cart.js
@@ -1,8 +1,91 @@
-import React from 'react'
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import { Remove, Add, NavigateBefore, LocalMall, RemoveCircle } from '@mui/icons-material';
+import { toast } from "react-hot-toast";
+import { useStateContext } from "../../../../context/StateContext";
+import { urlFor } from "../../../../lib/client";
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
+
   return (
-    <div>Cart</div>
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <NavigateBefore />
+          <span className="heading">Tu Carrito</span>
+          <span className="cart-num-items">({totalQuantities} producto/s)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <LocalMall style={{fontSize: "150px"}} />
+            <h4>Tu carrito de compras está vacío</h4>
+            <Link to="/store">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Seguir comprando
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 && cartItems.map((item) => (
+            <div className="product" key={item._id}>
+              <img src={urlFor(item?.image[0])} className="cart-product-image" />
+              <div className="item-desc">
+                <div className="flex top">
+                  <h5>{item.name}</h5>
+                  <h4>${item.price}</h4>
+                </div>
+                <div className="flex bottom">
+                  <div>
+                    <p className="quantity-desc">
+                      <span className="remove" onClick={() => toggleCartItemQuantity(item._id, "dec")}><Remove /></span>
+                      <span className="num">{item.quantity}</span>
+                      <span className="add" onClick={() => toggleCartItemQuantity(item._id, "inc")}><Add /></span>
+                    </p>
+                  </div>
+                  <button
+                    type="button"
+                    className="remove-item"
+                    onClick={() => onRemove(item)}
+                  >
+                    <RemoveCircle />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h4>Subtotal:</h4>
+              <h4>${totalPrice}</h4>
+            </div>
+            <div className="btn-container">
+              <button
+                type="button"
+                className="btn"
+                onClick=""
+              >
+                Pagar con Paypal
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
